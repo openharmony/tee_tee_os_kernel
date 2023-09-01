89064c35ba1c03014f7f9bf0fadeede346789953
suwanghw <wangsu14@huawei.com>
2023-09-01 15:44:37 +0800

    Modify the compilation process to adapt to OH
    Signed-off-by: suwanghw <wangsu14@huawei.com>

--- build/build_tee.sh
+++ build/build_tee.sh
@@ -7,32 +7,41 @@
 # IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
 # PURPOSE.
 # See the Mulan PSL v2 for more details.
+set -e
 
 # paths setting
-CHCORE_DIR=$(pwd)
-OH_TEE_FRAMEWORK_DIR=${CHCORE_DIR}/../tee_os_framework/
-OH_TEE_PREBUILD_DIR=${OH_TEE_FRAMEWORK_DIR}/prebuild/
-OH_TEE_HEADERS_DIR=${OH_TEE_PREBUILD_DIR}/tee-kernel-local-release/headers/
+OH_TOP_DIR=$1
+CHCORE_DIR=$2
+OH_TEE_FRAMEWORK_DIR=$3
+COMPILER_DIR=$4
+COMPILER_VER=$5
+if [[ -z "$OH_TOP_DIR" ]]; then
+    OH_TOP_DIR=$(pwd)/../../../..
+fi
+if [[ -z "$CHCORE_DIR" ]]; then
+    CHCORE_DIR=$(pwd)/..
+fi
+if [[ -z "$OH_TEE_FRAMEWORK_DIR" ]]; then
+    OH_TEE_FRAMEWORK_DIR=$(pwd)/../../tee_os_framework
+fi
+OH_TEE_PREBUILD_DIR=${OH_TEE_FRAMEWORK_DIR}/prebuild
+OH_TEE_HEADERS_DIR=${OH_TEE_PREBUILD_DIR}/tee-kernel-local-release/headers
 OH_TEE_LIBS_DIR=${OH_TEE_PREBUILD_DIR}/tee-kernel-local-release/libs/aarch64
-OH_TOP_DIR=${CHCORE_DIR}/../../../
-THIRD_PARTY=${OH_TOP_DIR}/third_party/
-
+THIRD_PARTY=${OH_TOP_DIR}/third_party
 # default compiler and version setting
 if [[ -z "$COMPILER_DIR" ]]; then
-    COMPILER_DIR=${OH_TOP_DIR}/prebuilts/clang/ohos/linux-x86_64/llvm/
+    COMPILER_DIR=${OH_TOP_DIR}/prebuilts/clang/ohos/linux-x86_64/llvm
 fi
 if [[ -z "$COMPILER_VER" ]]; then
     COMPILER_VER=15.0.4
 fi
-
 # clean framework
 cd ${OH_TEE_FRAMEWORK_DIR}/build
-./clean_framework.sh
-
+./clean_framework.sh ${OH_TEE_FRAMEWORK_DIR}
 # compile chcore
 cd ${CHCORE_DIR}
 rm -rf oh_tee
-make clean && make -j$(nproc) CHCORE_COMPILER_DIR=${COMPILER_DIR}
+make clean && make -j$(nproc) CHCORE_COMPILER_DIR=${COMPILER_DIR} OH_DIR=${OH_TOP_DIR}
 
 mkdir -p ramdisk-dir
 cp libc_shared.so ${CHCORE_DIR}/ramdisk-dir/libc_shared.so
@@ -50,14 +59,12 @@ mkdir -p ${OH_TEE_HEADERS_DIR}/libc
 mkdir -p ${OH_TEE_HEADERS_DIR}/sys
 cp -r ${CHCORE_DIR}/user/chcore-libc/musl-libc/install/include/* ${OH_TEE_HEADERS_DIR}/libc/
 cp ${CHCORE_DIR}/user/chcore-libs/sys-libs/libohtee/include/* ${OH_TEE_HEADERS_DIR}/sys/
-
 # go to framework and build it
 cd ${OH_TEE_FRAMEWORK_DIR}/build
-./build_framework.sh oh_64 ${CHCORE_DIR}/oh_tee ${COMPILER_DIR} ${COMPILER_VER}
-
+./build_framework.sh oh_64 ${CHCORE_DIR}/oh_tee ${COMPILER_DIR} ${COMPILER_VER} ${OH_TEE_FRAMEWORK_DIR} ${THIRD_PARTY}
 # compile again to put the apps into ramdisk-dir
 cd ${CHCORE_DIR}
 make clean
 mkdir -p ramdisk-dir
 cp oh_tee/apps/* ramdisk-dir
-make -j$(nproc)
+make -j$(nproc) OH_DIR=${OH_TOP_DIR}
